8aa42726c0298cdb638d824ecf4473604f3f6231
Jhon Trujillo Serrano <jhontruse@gmail.com>
2026-04-07 22:30:03 -0500

    Inicio de proyecto

---
+++ src/main/java/com/jhontruse/wsrestaurante/WsRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.jhontruse.wsrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsRestauranteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhontruse/wsrestaurante/WsRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhontruse.wsrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
