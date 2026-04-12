c437bcc34e820c5e386edf6916f0c42d9cd25056
Jhon Trujillo Serrano <jhontruse@gmail.com>
2026-04-12 17:35:21 -0500

    Implementar service de menú

--- src/main/java/com/jhontruse/wsrestaurante/service/IMenuService.java
+++ src/main/java/com/jhontruse/wsrestaurante/service/IMenuService.java
@@ -1,4 +1,36 @@
-package com.jhontruse.wsrestaurante.service.impl;
+package com.jhontruse.wsrestaurante.service;
+
+import com.jhontruse.wsrestaurante.model.entity.Menu;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface IMenuService {
+
+    List<Menu> findAll();
+
+    Optional<Menu> findMenuById(String id);
+
+    List<Menu> findMenuByNombre(String nombre);
+
+    List<Menu> findMenuByRuta(String ruta);
+
+    List<Menu> findMenuByIcono(String icono);
+
+    List<Menu> findMenuByOrden(Integer orden);
+
+    List<Menu> findMenuByActivo(boolean activo);
+
+    List<Menu> findMenuByNombreIsLike(String nombre);
+
+    List<Menu> findMenuByNombreStartingWith(String nombre);
+
+    Menu save(Menu menu);
+
+    Menu update(Menu menu);
+
+    void delete(Menu menu);
+
+    void deleteById(String id);
+
 }
--- src/main/java/com/jhontruse/wsrestaurante/service/impl/MenuService.java
+++ src/main/java/com/jhontruse/wsrestaurante/service/impl/MenuService.java
@@ -1,4 +1,163 @@
 package com.jhontruse.wsrestaurante.service.impl;
 
-public class MenuService {
+import com.jhontruse.wsrestaurante.exception.ResourceNotFoundException;
+import com.jhontruse.wsrestaurante.model.entity.Menu;
+import com.jhontruse.wsrestaurante.repository.IMenuRepository;
+import com.jhontruse.wsrestaurante.service.IMenuService;
+import com.jhontruse.wsrestaurante.util.validator.MenuValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MenuService implements IMenuService {
+
+    private final IMenuRepository iMenuRepository;
+
+    private final MenuValidator menuValidator;
+
+    @Override
+    public List<Menu> findAll() {
+        log.info("MenuService - findAll");
+        List<Menu> menus = iMenuRepository.findAll().stream().sorted(Comparator.comparing(Menu::getOrden)).toList();
+        log.info("menus {}", menus);
+        return menus;
+    }
+
+    @Override
+    public Optional<Menu> findMenuById(String id) {
+        log.info("MenuService - findMenuById");
+        log.info("id {}", id);
+        Optional<Menu> menuOpt = iMenuRepository.findMenuById(id).stream().findFirst();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByNombre(String nombre) {
+        log.info("MenuService - findMenuByNombre");
+        log.info("nombre {}", nombre);
+        List<Menu> menuOpt = iMenuRepository.findMenuByNombre(nombre).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByRuta(String ruta) {
+        log.info("MenuService - findMenuByRuta");
+        log.info("ruta {}", ruta);
+        List<Menu> menuOpt = iMenuRepository.findMenuByRuta(ruta).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByIcono(String icono) {
+        log.info("MenuService - findMenuByIcono");
+        log.info("icono {}", icono);
+        List<Menu> menuOpt = iMenuRepository.findMenuByIcono(icono).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByOrden(Integer orden) {
+        log.info("MenuService - findMenuByOrden");
+        log.info("orden {}", orden);
+        List<Menu> menuOpt = iMenuRepository.findMenuByOrden(orden).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByActivo(boolean activo) {
+        log.info("MenuService - findMenuByActivo");
+        log.info("activo {}", activo);
+        List<Menu> menuOpt = iMenuRepository.findMenuByActivo(activo).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByNombreIsLike(String nombre) {
+        log.info("MenuService - findMenuByNombreIsLike");
+        log.info("nombre {}", nombre);
+        List<Menu> menuOpt = iMenuRepository.findMenuByNombreIsLike(nombre).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public List<Menu> findMenuByNombreStartingWith(String nombre) {
+        log.info("MenuService - findMenuByNombreStartingWith");
+        log.info("nombre {}", nombre);
+        List<Menu> menuOpt = iMenuRepository.findMenuByNombreStartingWith(nombre).stream().toList();
+        log.info("menuOpt {}", menuOpt);
+        if (menuOpt.isEmpty()) {
+            throw new ResourceNotFoundException("No se encontraron registros de Menu");
+        }
+        return menuOpt;
+    }
+
+    @Override
+    public Menu save(Menu menu) {
+        log.info("MenuService - save");
+        menuValidator.validarParaCrear(menu);
+        String uuid = UUID.randomUUID().toString();
+        log.info("uuid {}", uuid);
+        menu.setId(uuid);
+        log.info("menu {}", menu);
+        Menu savedMenu = iMenuRepository.procedureSaveMenu(menu);
+        log.info("savedMenu {}", savedMenu);
+        return savedMenu;
+    }
+
+    @Override
+    public Menu update(Menu menu) {
+        log.info("MenuService - update");
+        menuValidator.validarParaActualizar(menu);
+        Menu updateMenu = iMenuRepository.procedureUpdateMenu(menu);
+        log.info("updateMenu {}", updateMenu);
+        return updateMenu;
+    }
+
+    @Override
+    public void delete(Menu menu) {
+        menuValidator.validarParaEliminar(menu);
+        iMenuRepository.deleteById(menu.getId());
+    }
+
+    @Override
+    public void deleteById(String id) {
+        menuValidator.validarParaEliminar(id);
+        iMenuRepository.deleteById(id);
+    }
 }
